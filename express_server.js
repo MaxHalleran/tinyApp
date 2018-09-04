274a9252d8a72de680249cef1ba1248163768bcd
Max <MaxHalleran@gmail.com>
2018-09-04 22:25:33 +0000

    I have the project able to take a form and to generate a random 6 letter string

--- express_server.js
+++ express_server.js
@@ -4,6 +4,11 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 var PORT = 8080; //default port 8080
 
+function generateRandomString() {
+  let randomID = Math.random().toString(36).substring(2, 8);
+  return randomID;
+}
+
 app.set("view engine", "ejs");
 
 var urlDatabase = {
@@ -12,27 +17,27 @@ var urlDatabase = {
 };
 
 app.get("/", (req, res) => {
-  res.render("index");
+  let templateVars = { urls: urlDatabase};
+  res.render("urls_index", templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase};
-  res.render("/urls_index", templateVars);
+  res.render("urls_index", templateVars);
 });
 
 app.get('/urls/new', (req, res) => {
-res.render('urls_new');
+  res.render('urls_new');
 });
 
-
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('ok');
+});
 
 app.get('urls/:id', (req, res) => {
   let templateVars = {
