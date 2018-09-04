3ffbe6f13f3f43161f71eb3052cdab8a6a96096c
Max <MaxHalleran@gmail.com>
2018-09-04 21:24:18 +0000

    My project in all it's glory, Should have commited starting at the beginning but will begin commiting now.

---
+++ express_server.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+var PORT = 8080; //default port 8080
+
+app.set("view engine", "ejs");
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xk": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase};
+  res.render("/urls_index", templateVars);
+});
+
+app.get('/urls/new', (req, res) => {
+res.render('urls_new');
+});
+
+
+
+app.get('urls/:id', (req, res) => {
+  let templateVars = {
+    shortURL: req.params.id,
+    longURL: urlDatabase[req.params.id]
+  };
+  res.render('urls_show', templateVars);
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
