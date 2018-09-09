bad96c559a667ffbd46fcd26831891c754c1e13f
Max <MaxHalleran@gmail.com>
2018-09-09 05:54:21 +0000

    Prepping TinyApp to go live

--- express_server.js
+++ express_server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
 const PORT = 8080; //default port 8080
 const cookieSession = require('cookie-session');
@@ -8,50 +7,46 @@ const router = require('./routes/urls.js');
 const utility = require('./lib/utility.js');
 const urlDatabase = require('./lib/urlDatabase.js');
 const usersDatabase = require('./lib/usersDatabase.js');
+const methodOverride = require('method-override');
+const app = express();
 
+app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded({extended: true}));
-
 app.use(cookieSession({
   name: 'session',
   keys: ['suppalightohousegottariot'],
-
-  // Cookie Options
-  maxAge: 24 * 60 * 60 * 1000 // 24 hours
-}))
-
+  maxAge: 24 * 60 * 60 * 1000
+}));
 app.set("view engine", "ejs");
 app.use('/urls', router);
 
-app.get('/register', (req, res) => {
-  if (utility.logStatus(req.session)) {
-    res.redirect('/');
-  }
-  let retry = (req.statusCode)
-  console.log('Working on register');
-  let templateVars = {
-    logStat: utility.logStatus(req.session),
-    user_id: utility.setTemplateVars(req.session.user_id, usersDatabase)
-  }
-  res.render('register',templateVars);
-});
-
-app.post('/register', (req, res) => {
-  console.log(usersDatabase.find(u => u.email === req.body.email));
-  if ((req.body.email === "" || req.body.username === "" || req.body.password === "") || !(usersDatabase.find(u => u.email === req.body.email))) {
-    res.redirect('/error/invalidEmail');
-  } else {
-    let tempID = utility.validateNumber(utility.generateRandomString(), usersDatabase);
-    usersDatabase.push({
-      id: tempID,
-      username: req.body.username,
-      email: req.body.email,
-      password: bcrypt.hashSync(req.body.password, 10),
-      collection: []
-    });
-    req.session.user_id = tempID;
-    res.redirect('/urls');
-  }
-});
+app.route('/register')
+  .get((req, res) => {
+    if (utility.logStatus(req.session)) {
+      res.redirect('/');
+    }
+    let templateVars = {
+      logStat: utility.logStatus(req.session),
+      user_id: utility.setTemplateVars(req.session.user_id, usersDatabase)
+    };
+    res.render('register',templateVars);
+  })
+  .post((req, res) => {
+    if ((req.body.email === "" || req.body.username === "" || req.body.password === "") || (usersDatabase.find(u => u.email === req.body.email))) {
+      res.redirect('/error/invalidEmail');
+    } else {
+      let tempID = utility.validateNumber(utility.generateRandomString(), usersDatabase);
+      usersDatabase.push({
+        id: tempID,
+        username: req.body.username,
+        email: req.body.email,
+        password: bcrypt.hashSync(req.body.password, 10),
+        collection: []
+      });
+      req.session.user_id = tempID;
+      res.redirect('/urls');
+    }
+  });
 
 app.get("/", (req, res) => {
   if (utility.logStatus(req.session)) {
@@ -70,27 +65,27 @@ app.get('/error/:error', (req, res) => {
   res.render('error', templateVars);
 });
 
-app.get('/login', (req, res) => {
-  if (utility.logStatus(req.session)) {
-    res.redirect('/');
-  } else {
-    let templateVars = {
-      logStat: false,
-      user_id: false
-    };
-    res.render('login', templateVars);
-  }
-});
-
-app.post('/login', (req, res) => {
-  let login = utility.validateLogin(req.body.username, req.body.password, usersDatabase);
-  if (!login) {
-    res.redirect('/error/invalidLogin');
-  } else {
-    req.session.user_id = login;
-    res.redirect('/');
-  }
-});
+app.route('/login')
+  .get((req, res) => {
+    if (utility.logStatus(req.session)) {
+      res.redirect('/');
+    } else {
+      let templateVars = {
+        logStat: false,
+        user_id: false
+      };
+      res.render('login', templateVars);
+    }
+  })
+  .post((req, res) => {
+    let login = utility.validateLogin(req.body.username, req.body.password, usersDatabase);
+    if (!login) {
+      res.redirect('/error/invalidLogin');
+    } else {
+      req.session.user_id = login;
+      res.redirect('/');
+    }
+  });
 
 app.post('/logout', (req, res) => {
   req.session = null;
--- routes/urls.js
+++ routes/urls.js
@@ -22,68 +22,67 @@ router.get("/", (req, res) => {
 }
 });
 
-router.get('/new', (req, res) => {
-  if (!utility.logStatus(req.session)) {
-    res.redirect('/');
-  } else {
-    let templateVars = {
-      logStat: utility.logStatus(req.session),
-      user_id: utility.setTemplateVars(req.session.user_id, usersDatabase)
+router.route('/new')
+  .get((req, res) => {
+    if (!utility.logStatus(req.session)) {
+      res.redirect('/');
+    } else {
+      let templateVars = {
+        logStat: utility.logStatus(req.session),
+        user_id: utility.setTemplateVars(req.session.user_id, usersDatabase)
+      };
+      res.render('urls_new', templateVars);
     }
-    res.render('urls_new', templateVars);
-  }
-});
+  })
+  .post((req, res) => {
+    if (!utility.logStatus(req.session)) {
+      res.redirect('/register');
+    } else {
+      let tempID = utility.validateNumber(utility.generateRandomString(), urlDatabase);
+      urlDatabase[tempID] = req.body.longUrl;
+      (usersDatabase[usersDatabase.findIndex(u => u.id === req.session.user_id)].collection).push(tempID);
+      res.Location = `http://localhost:8080/urls/${tempID}`;
+      res.redirect(`http://localhost:${PORT}/urls/${tempID}`);
+    }
+  });
 
-router.post('/new', (req, res) => {
-  if (!utility.logStatus(req.session)) {
-    res.redirect('/register');
-  } else {
-    let tempID = utility.validateNumber(utility.generateRandomString(), urlDatabase);
-    urlDatabase[tempID] = req.body.longUrl;
-    (usersDatabase[usersDatabase.findIndex(u => u.id === req.session.user_id)].collection).push(tempID);
-    res.Location = `http://localhost:8080/urls/${tempID}`;
-    res.redirect(`http://localhost:${PORT}/urls/${tempID}`);
-  }
-});
-
-router.post('/:shortUrl/delete', (req, res) => {
-  if (utility.logStatus(!req.session)) {
-    res.redirect(400, '/');
-  }
-  if (utility.isOwner(req.params.shortUrl, req.session.user_id, usersDatabase)) {
-    let index = usersDatabase[usersDatabase.findIndex(u => u.id === req.session.user_id)].collection.findIndex(e => e === req.params.shortUrl);
-    usersDatabase[usersDatabase.findIndex(u => u.id === req.session.user_id)].collection.splice(index, 1);
-    delete urlDatabase[req.params.shortUrl];
-    res.redirect(`http://localhost:${PORT}/urls`);
-  } else {
-    res.redirect(400, '/');
-  }
-});
-
-router.post('/:shortUrl/update', (req, res) => {
-  if (utility.isOwner(req.params.shortUrl, req.session.user_id, usersDatabase)) {
-    urlDatabase[req.params.shortUrl] = req.body.longUrl;
-    res.Location = `http://localhost:${PORT}`;
-    res.redirect(`http://localhost:${PORT}`);
-  } else {
-    res.redirect(400, '/');
-  }
-});
-
-router.get('/:id', (req, res) => {
-  if (!utility.logStatus(req.session)) {
-    res.redirect('/');
-  } else if (utility.isOwner(req.params.id, req.session.user_id, usersDatabase)) {
-    let templateVars = {
-      logStat: utility.logStatus(req.session),
-      user_id: utility.setTemplateVars(req.session.user_id, usersDatabase),
-      shortUrl: req.params.id,
-      longUrl: urlDatabase[req.params.id]
-    };
-    res.render('urls_show', templateVars);
-  } else {
-    res.redirect('/');
-  }
-});
+router.route('/:shortUrl?')
+  .get((req, res) => {
+    if (!utility.logStatus(req.session)) {
+      res.redirect('/');
+    } else if (utility.isOwner(req.params.shortUrl, req.session.user_id, usersDatabase)) {
+      let templateVars = {
+        logStat: utility.logStatus(req.session),
+        user_id: utility.setTemplateVars(req.session.user_id, usersDatabase),
+        shortUrl: req.params.shortUrl,
+        longUrl: urlDatabase[req.params.shortUrl]
+      };
+      res.render('urls_show', templateVars);
+    } else {
+      res.redirect('/');
+    }
+  })
+  .delete((req, res) => {
+    if (utility.logStatus(!req.session)) {
+      res.redirect(400, '/');
+    }
+    if (utility.isOwner(req.params.shortUrl, req.session.user_id, usersDatabase)) {
+      let index = usersDatabase[usersDatabase.findIndex(u => u.id === req.session.user_id)].collection.findIndex(e => e === req.params.shortUrl);
+      usersDatabase[usersDatabase.findIndex(u => u.id === req.session.user_id)].collection.splice(index, 1);
+      delete urlDatabase[req.params.shortUrl];
+      res.redirect(`http://localhost:${PORT}/urls`);
+    } else {
+      res.redirect(400, '/');
+    }
+  })
+  .put((req, res) => {
+    if (utility.isOwner(req.params.shortUrl, req.session.user_id, usersDatabase)) {
+      urlDatabase[req.params.shortUrl] = req.body.longUrl;
+      res.Location = `http://localhost:${PORT}`;
+      res.redirect(`http://localhost:${PORT}`);
+    } else {
+      res.redirect(400, '/');
+    }
+  });
 
 module.exports = router;
